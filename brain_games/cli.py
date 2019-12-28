1901a256159c22bf5860e37dfc6f60379bd31500
mellyssy <ksenyatokareva@gmail.com>
2019-12-28 18:18:39 +0300

    step 3 additions

---
+++ brain_games/cli.py
@@ -0,0 +1,11 @@
+import prompt
+
+def run():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+def main():
+    run()
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+from brain_games.cli import run
+
 def main():
     print('Welcome to the Brain Games!')
+    print()
+    run()
 
 if __name__ == '__main__':
     main()
