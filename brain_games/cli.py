414af206e74b161b8c3642d1645afaba2985de0b
mellyssy <ksenyatokareva@gmail.com>
2019-12-30 20:35:18 +0300

    add flake8, fix errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,14 @@
 import prompt
 
+
 def run():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
+
 def main():
     run()
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 from brain_games.cli import run
 
+
 def main():
     print('Welcome to the Brain Games!')
     print()
     run()
+
 
 if __name__ == '__main__':
     main()
